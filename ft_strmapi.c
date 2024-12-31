685f7d1616cb892c83faa7ef6895e73c342c2618
FranciscoLi2000 <francisco2000li@icloud.com>
2024-12-31 17:24:37 +0000

    42Barcelona

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -3,3 +3,25 @@
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char    *str;
+    int i;
+    int len;
+
+    if (!s || !f)
+        return (NULL);
+    len = 0;
+    while (s[len] != '\0')
+        len++;
+    str = malloc((len + 1) * sizeof(char));
+    if (!str)
+        return (NULL);
+    i = 0;
+    while (i < len)
+    {
+        str = f(i, s[i]);
+        i++;
+    }
+    str[i] = '\0';
+    return (str);
+}
