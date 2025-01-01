f339734bfa993caa5c42560eab17c308c702bbb0
FranciscoLi2000 <francisco2000li@icloud.com>
2025-01-01 16:15:53 +0000

    20250101

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "libft.h"
+
+void    ft_putnbr_fd(int n, int fd)
+{
+    if (n == -2147483648)
+    {
+        ft_putstr_fd("2147483648", fd);
+        return ;
+    if (n < 0)
+    {
+        ft_putchar_fd('-', fd);
+        n = -n;
+    }
+    if (n >= 0)
+        ft_putnbr_fd(n / 10, fd);   // 递归 
+    ft_putchar_fd(n % 10 + '0', fd);
+}
