6ad4d6e026d5d89280e731acce5f254757552d10
FranciscoLi2000 <liyufeng577@gmail.com>
2025-08-15 08:43:22 +0000

    libft

--- additional/ft_substr.c
+++ additional/ft_substr.c
@@ -1,19 +1,25 @@
 #include "libft.h"
 
-char *ft_substr(const char *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    unsigned int    i;
-    char        *res;
+	size_t	i;
+	char	*res;
 
-    if (len > ft_strlen(s + start))
-        len = ft_strlen(s + start);
-    res = malloc((len + 1) * sizeof(char));
-    if (!res)
-        return (NULL);
-    i = start;
-    while (i < len)
-    {
-        res[i] = s[i];
-        i++;
-    }
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	res = (char *)malloc(len + 1);
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		res[i] = s[start + i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
 }
