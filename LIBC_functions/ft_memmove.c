f328d698de2a69c18d495ad39102aec7e18ebba9
FranciscoLi2000 <liyufeng577@gmail.com>
2025-11-13 15:38:44 +0000

    learn LIBC functions implementations

---
+++ LIBC_functions/ft_memmove.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char	*tmp_src;
+	char	*tmp_dst;
+	size_t	i;
+
+	if (!dst && !src)
+		return (NULL);
+	tmp_src = (char *)src;
+	tmp_dst = (char *)dst;
+	i = 0;
+	if (tmp_dst > tmp_src)
+		while (len-- > 0)
+			tmp_dst[len] = tmp_src[len];
+	else
+	{
+		while (i++ < len)
+			tmp_dst[i] = tmp_src[i];
+	}
+	return (dst);
+}
