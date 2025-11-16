c8c7b9fb93cc3866b4bc4053842921c78be2c425
FranciscoLi2000 <liyufeng577@gmail.com>
2025-11-16 11:26:37 +0000

    Get Next Line -- read a whole line in a file

--- Get_Next_Line/fill_line_buffer.c
+++ Get_Next_Line/fill_line_buffer.c
@@ -6,11 +6,18 @@
 /*   By: yufli <yufli@student.42barcelona.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 21:58:34 by yufli             #+#    #+#             */
-/*   Updated: 2025/11/14 21:58:37 by yufli            ###   ########.fr       */
+/*   Updated: 2025/11/16 10:58:40 by yufli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static char	*free_and_null(char *p)
+{
+	if (p)
+		free(p);
+	return (NULL);
+}
 
 char	*fill_line_buffer(int fd, char *left)
 {
@@ -18,7 +25,24 @@ char	*fill_line_buffer(int fd, char *left)
 	ssize_t	rbytes;
 	char	*tmp;
 
-	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	buf = malloc(BUFFER_SIZE + 1);
 	if (!buf)
 		return (free_and_null(left));
+	rbytes = 1;
+	while (rbytes > 0 && !(left && ft_strchr(left, '\n')))
+	{
+		rbytes = read(fd, buf, BUFFER_SIZE);
+		if (rbytes <= 0)
+			break ;
+		buf[rbytes] = '\0';
+		if (!left)
+			left = ft_strdup(buf);
+		tmp = ft_strjoin(left, buf);
+		free(left);
+		left = tmp;
+	}
+	free(buf);
+	if (rbytes < 0)
+		return (free_and_null(left));
+	return (left);
 }
--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: yufli <yufli@student.42barcelona.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 21:54:46 by yufli             #+#    #+#             */
-/*   Updated: 2025/11/14 21:56:23 by yufli            ###   ########.fr       */
+/*   Updated: 2025/11/16 10:35:29 by yufli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,13 @@
 
 # include "libft.h"
 # include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
 
 # define BUFFER_SIZE 42
 
+char	*fill_line_buffer(int fd, char *left);
+char	*extract_line(char **left);
+char	*get_next_line(int fd);
 
 #endif
