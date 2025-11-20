21c2ae4ccb834dc34b67e5b8da3f98de9ca30a37
FranciscoLi2000 <liyufeng577@gmail.com>
2025-11-20 13:39:29 +0000

    C 01

---
+++ C01/ft_ft.c
@@ -0,0 +1,17 @@
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int	x;
+
+	x = 5;
+	printf("%i\n", x);
+	ft_ft(&x);
+	printf("%i\n", x);
+	return (0);
+}
---
+++ C01/ft_rev_int_tab.c
@@ -0,0 +1,16 @@
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	tmp;
+	int	i;
+
+	if (!tab || size <= 1)
+		return ;
+	i = 0;
+	while (i < size / 2)
+	{
+		tmp = tab[i];
+		tab[i] = tab[size - i - 1];
+		tab[size - i - 1] = tmp;
+		i++;
+	}
+}
