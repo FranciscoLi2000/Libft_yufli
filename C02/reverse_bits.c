b9d06332b57a664426a9e1012dd537302619c35e
FranciscoLi2000 <liyufeng577@gmail.com>
2025-11-20 17:52:21 +0000

    C 03

---
+++ C02/reverse_bits.c
@@ -0,0 +1,14 @@
+unsigned char	reverse_bits(unsigned char octet)
+{
+	unsigned char	result;
+	int			i;
+
+	i = 0;
+	result = 0;
+	while (i < 8)
+	{
+		result |= ((octet >> i) & 1) << (7 - i);
+		++i;
+	}
+	return (result);
+}
